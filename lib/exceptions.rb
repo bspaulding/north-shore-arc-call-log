4dcb0f0dad5cf7be1b8090a21fafb11648b4321b
Bradley Spaulding <brad.spaulding@gmail.com>
2009-03-01 19:09:19 -0500

    added header comments

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,3 +1,6 @@
+# Module CallLogExceptions 
+# defines application specific errors, which extend from the appropriate ruby base class.
+# Author: Bradley J. Spaulding
 module CallLogExceptions
   # InvalidSpreadsheet
   # Raised when a spreadsheet is detected to be invalid.
